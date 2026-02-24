e41e314daa8fc3b5f12c13bbe0759da694475208
ys1454-tech <ys1454@srmist.edu.in>
2026-02-24 22:09:06 +0530

    Base application setup

---
+++ src/palindromechecker/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+package palindromechecker;
+
+public class UseCase1PalindromeCheckerApp {
+
+    // Main method - Entry point of the application
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("     WELCOME TO PALINDROME CHECKER     ");
+        System.out.println("====================================");
+
+        System.out.println("Application Name    : Palindrome Checker");
+        System.out.println("Application Version : 1.0.0");
+        System.out.println("Developed Using     : Java");
+        System.out.println("------------------------------------------");
+
+        System.out.println("System is ready to proceed to next use case...");
+    }
+}
